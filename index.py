629bdd0599936cc861a1df754ea4a4c0135eedeb
Yves MAKWAMBALA <yvesmak1@gmail.com>
2022-11-13 11:40:47 +0100

    extraire les données d'un site web

--- index.py
+++ index.py
@@ -1,3 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
-requests.get()
+url = "https://www.gov.uk/search/news-and-communications"
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+titres_bs = soup.find_all('a')
+
+titres = []
+
+for titre in titres_bs:
+    titres.append(titre.string)
+    print(titres)
